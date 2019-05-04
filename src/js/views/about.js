f8ed671d58eb51091c8c5995e347ef020fd8f09e
ansuz <ansuz@transitiontech.ca>
2019-05-04 17:22:59 +0200

    update about text again

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -10,7 +10,7 @@ module.exports.route = function (req, res) {
     box.innerHTML = Marked("This tool provides [provably fair](https://en.wikipedia.org/wiki/Provably_fair) random functions using cryptography in your browser.\n\n"
     + "You can use it to play games with friends.\n\n"
     + "Set your name in the 'whoami' tab, then use the 'dicebag' to roll flip coins or roll dice.\n\n"
-    + "It only uses a simple server to relay messages, but everything else is peer-to-peer"
+    + "It uses a simple server to relay messages, but everything else is peer-to-peer.\n\n"
     + "This project was inspired by [Cryptomancer](http://cryptorpg.com), _a tabletop role-playing game made for hackers, by hackers_.\n\n"
     + "Its source code is available [on GitHub](https://github.com/ansuz/beacon).\n\n"
     );

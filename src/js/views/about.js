3f2b5e18b8587e1eadc7b824406db2e3c73b5f8d
ansuz <ansuz@transitiontech.ca>
2019-05-05 22:22:25 +0200

    I don't know why friends was in parentheses

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -8,7 +8,7 @@ module.exports.route = function (req, res) {
 
     var box = h('div.copy');
     box.innerHTML = Marked("This tool provides  private chat rooms with [provably fair](https://en.wikipedia.org/wiki/Provably_fair) random functions in your browser thanks to a little bit of cryptography.\n\n"
-    + "You can use it to play games with some assurance that nobody is reading your messages, and that none of your _'friends'_ are cheating on their rolls.\n\n"
+    + "You can use it to play games with some assurance that nobody is reading your messages, and that nobody is cheating on their dice rolls or coin flips.\n\n"
     + "It uses a simple server to relay messages, but everything else is peer-to-peer.\n\n"
     + "Set your name in the 'settings' tab, and then use the 'dicebag' to roll flip coins or roll dice.\n\n"
     + "This project was inspired by [Cryptomancer](http://cryptorpg.com), _a tabletop role-playing game made for hackers, by hackers_.\n\n"

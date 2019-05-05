9d635a6f55dd9ca24cbd7aec2c36e609769d779a
ansuz <ansuz@transitiontech.ca>
2019-05-05 15:13:43 +0200

    rename 'whoami' to 'settings'

--- src/js/views/index.js
+++ src/js/views/index.js
@@ -1,7 +1,7 @@
 var routes = module.exports;
 routes[undefined] = routes.about = require("./about").route;
 routes.dicebag = require("./dicebag").route;
-routes.whoami = require("./whoami").route;
+routes.settings = require("./settings").route;
 routes.four04 = require("./four04").route;
 
 
